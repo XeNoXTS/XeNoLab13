a11f6d1c07161d91c72aa8d2911a05be9894df0f
XeNoXTS <palmyunvisate2003@gmail.com>
2023-01-10 15:30:22 +0700

    Lab13_4 Completed

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+template <class Type>
+
+void mySwap(Type &,Type &); 
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +34,10 @@ int main(){
 	return 0;
 }
 
+template <class T>
+void mySwap(T &x,T &y)
+{
+	T i = x;
+	x = y;
+	y = i;
+}
